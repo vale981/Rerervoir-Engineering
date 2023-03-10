b08a4d74a05a19668dd88fed365fdd3ef67669e8
DrWatson <no@mail>
2023-03-10 18:41:00 +0000

    File setup by DrWatson

---
+++ test/runtests.jl
@@ -0,0 +1,17 @@
+using DrWatson, Test
+@quickactivate "Reservoir Engineering"
+
+# Here you include files using `srcdir`
+# include(srcdir("file.jl"))
+
+# Run test suite
+println("Starting tests")
+ti = time()
+
+@testset "Reservoir Engineering tests" begin
+    @test 1 == 1
+end
+
+ti = time() - ti
+println("\nTest took total time of:")
+println(round(ti/60, digits = 3), " minutes")
